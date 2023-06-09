6eb9d1104c737ad2f355733239d31a3e63b0ddc2
Chu Thị Yên <119092877+CHUTHIYE@users.noreply.github.com>
2023-06-09 16:15:09 +0700

    -

---
+++ Lab2_slot9.1/Ex/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project9
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("SIN Validator");
+            Console.WriteLine("==============");
+
+            while (true)
+            {
+                Console.Write("SIN (0 to quit): ");
+                string input = Console.ReadLine();
+
+                if (input == "0")
+                {
+                    Console.WriteLine("Have a Nice Day!");
+                    break;
+                }
+
+                if (ValidateSIN(input))
+                {
+                    Console.WriteLine("This is a valid SIN.");
+                }
+                else
+                {
+                    Console.WriteLine("This is not a valid SIN.");
+                }
+            }
+        }
+
+        public static bool ValidateSIN(string sin)
+        {
+            int sumEven = 0;
+            int sumOdd = 0;
+
+            for (int i = sin.Length - 2; i >= 0; i -= 2)
+            {
+                int digit = int.Parse(sin[i].ToString());
+                digit *= 2;
+
+                while (digit > 0)
+                {
+                    sumEven += digit % 10;
+                    digit /= 10;
+                }
+            }
+
+            for (int i = sin.Length - 3; i >= 0; i -= 2)
+            {
+                int digit = int.Parse(sin[i].ToString());
+                sumOdd += digit;
+            }
+
+            int total = sumEven + sumOdd;
+            int nextHighest = (total / 10 + 1) * 10;
+            int difference = nextHighest - total;
+            int checkDigit = int.Parse(sin[sin.Length - 1].ToString());
+
+            return difference == checkDigit;
+        }
+
+    }
+
+}
+
---
+++ Lab2_slot9.1/ObjectArray/ObjectArray.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ObjectArray arr = new ObjectArray();
+            arr.Run();
+            Console.ReadLine();
+        }
+    }
+}
